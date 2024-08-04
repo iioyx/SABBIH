e56bbf2870aa74b030809609caebf76527cf8f44
Yousef ibrahim <108943127+iioyx@users.noreply.github.com>
2024-08-04 19:24:05 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,36 +3,33 @@ let resetBtn = document.getElementById('reset-btn');
 let icons = document.getElementsByClassName('icon');
 
 for (let i = 0; i < itembtns.length; i++) {
-    let counter = 0;
+    itembtns[i].counter = 0; 
+
     itembtns[i].addEventListener('click', function() {
-        counter++;
-        itembtns[i].innerHTML = counter;
+        this.counter++;
+        this.innerHTML = this.counter;
     });
 
     icons[i].addEventListener('click', function() {
-        counter = 0;
-        itembtns[i].innerHTML = counter;
+        itembtns[i].counter = 0;
+        itembtns[i].innerHTML = 0;
     });
 }
 
 resetBtn.addEventListener('click', function() {
-   Swal.fire({
-    icon:"warning",
-  title: "هل أنت متأكد انك تريد تصفير العدادات ؟",
-  showDenyButton: false,
-  showCancelButton: true,
-  confirmButtonText: "Yes",
-  denyButtonText: `Don't save`
-}).then((result) => {
-  if (result.isConfirmed) {
-    Swal.fire("! تم تصفير العدادات بنجاح ", "", "success");
-    for (let i = 0; i < itembtns.length; i++) {
-        itembtns[i].innerHTML = 0;
-    }
-  } else if (result.isDenied) {
-    Swal.fire("Changes are not saved", "", "info");
-  }
+    Swal.fire({
+        icon: "warning",
+        title: "هل أنت متأكد انك تريد تصفير العدادات؟",
+        showCancelButton: true,
+        confirmButtonText: "نعم",
+        cancelButtonText: "إلغاء"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire("تم تصفير العدادات بنجاح!", "", "success");
+            for (let i = 0; i < itembtns.length; i++) {
+                itembtns[i].counter = 0;
+                itembtns[i].innerHTML = 0;
+            }
+        }
+    });
 });
-});
-
-
